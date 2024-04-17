efe2a15b843b2ab3195dd42e8aede93ea6f15302
Student Free <andreevdamir266@gmail.com>
2024-04-17 11:44:37 +0300

    коммит3

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,8 @@
-from src.masks import mask_account_number, mask_card_number, mask_info
+from src.masks import mask_info, format_date
 
-masked_infos = mask_info(input())
-print(masked_infos)
+masked_info = mask_info(input())
+print(masked_info)
 
+date_str = "2018-07-11T02:26:18.671407"
+formatted_date = format_date(date_str)
+print(formatted_date)
