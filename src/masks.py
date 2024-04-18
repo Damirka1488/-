6fcd67bcf1bde6a765615e2be848d2aa67be98a7
Student Free <andreevdamir266@gmail.com>
2024-04-18 10:42:59 +0300

    коммит23

--- src/masks.py
+++ src/masks.py
@@ -14,21 +14,3 @@ def mask_card_number(card_number: str) -> str:
     last_four = card_number[-4:]
     masked_number = first_six + " " + average_two + "** **** " + last_four
     return masked_number
-
-
-def mask_info(info: str) -> str:
-    """Возвращает номер карты или номер счёта"""
-    if info.startswith('Счет'):
-        return mask_account_number(info.split()[1])
-    elif info.startswith('Mastercard'):
-        return 'Mastercard ' + mask_card_number(info.split()[1])
-    elif info.startswith('Maestro'):
-        return 'Maestro ' + mask_card_number(info.split()[1])
-    else:
-        return ' '.join([info.split()[0]] + [mask_card_number(num) if i == 1 else num for i, num in enumerate(info.split
-                                                                                                              ()[1:])])
-
-def format_date(date_str):
-    date_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
-    formatted_date = date_obj.strftime("%d.%m.%Y")
-    return formatted_date
---
+++ src/widget.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from src.masks import mask_account_number, mask_card_number
+
+
+def mask_info(info: str) -> str:
+    """Возвращает номер карты или номер счёта"""
+    if info.startswith("Счет"):
+        return mask_account_number(info.split()[1])
+    elif info.startswith("Mastercard"):
+        return "Mastercard " + mask_card_number(info.split()[1])
+    elif info.startswith("Maestro"):
+        return "Maestro " + mask_card_number(info.split()[1])
+    else:
+        return " ".join(
+            [info.split()[0]] + [mask_card_number(num) if i == 1 else num for i, num in enumerate(info.split()[1:])]
+        )
+
+
+def format_date(date_str: str) -> str:
+    """Получает дату в формате 2018-07-11T02:26:18.671407 и выводит 11.07.2018"""
+    date_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
+    formatted_date = date_obj.strftime("%d.%m.%Y")
+    return formatted_date
