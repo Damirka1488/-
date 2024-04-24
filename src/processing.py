cad1f8dcc0043d9fb17e3a566129ab66ff71f5f5
Student Free <andreevdamir266@gmail.com>
2024-04-24 14:34:46 +0300

    коммит10.1

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,21 @@
-def filter_by_state_and_date(list_of_dicts, state="EXECUTED", date=None):
-    """Фильтрует список словарей по состоянию и дате."""
+from typing import Dict, List, Optional
 
+
+def filter_by_state_and_date(
+    list_of_dicts: List[Dict], state: str = "EXECUTED", date: Optional[str] = None
+) -> (List)[Dict]:
+    """Фильтрует список словарей по состоянию и дате."""
     filtered_list = []
     for d in list_of_dicts:
-        if d.get('state') == state:
+        if d.get("state") == state:
             if date:
-                if d.get('date')[:10] == date[:10]:
+                if d.get("date")[:10] == date[:10]:
                     filtered_list.append(d)
             else:
                 filtered_list.append(d)
     return filtered_list
 
 
-def sort_by_date(data, reverse=True):
+def sort_by_date(data: list[dict], reverse: bool = True) -> list[dict]:
     """Сортирует список словарей по убыванию даты."""
-    return sorted(data, key=lambda x: x['date'], reverse=reverse)
+    return sorted(data, key=lambda x: x["date"], reverse=reverse)
