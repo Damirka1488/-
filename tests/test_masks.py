684218e3f2bda714270bbb9c4fbfdc92202fc377
Student Free <andreevdamir266@gmail.com>
2024-05-02 10:27:31 +0300

    коммит10.2(2)

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,7 +3,7 @@ from src.masks import mask_account_number, mask_card_number
 
 
 @pytest.mark.parametrize("account_number, expected_result", [
-    ("1234567890", "Счет 7890"),
+    ("1234567890", "Счет **7890"),
     ("9876543210", "Счет **3210"),
     ("0000123456", "Счет **3456"),
 ])
@@ -12,7 +12,7 @@ def test_mask_account_number(account_number, expected_result):
 
 
 @pytest.mark.parametrize("card_number, expected_result", [
-    ("1234567890123456", "1234 56 **** 3456"),
+    ("1234567890123456", "1234 56** **** 3456"),
     ("9876543210987654", "9876 54** **** 7654"),
     ("0000111122223333", "0000 11** **** 3333"),
 ])
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -2,8 +2,8 @@ from src.widget import mask_info, format_date
 
 
 def test_mask_info():
-    assert mask_info("Счет 1234567890") == "Счет 7890"
-    assert mask_info("Maestro 1234567890123456") == "Maestro 1234 56 **** 3456"
+    assert mask_info("Счет 1234567890") == "Счет **7890"
+    assert mask_info("Maestro 1234567890123456") == "Maestro 1234 56** **** 3456"
 
 
 def test_format_date():
