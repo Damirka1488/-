b25122af6b9f7c4ecf694dfcbc5c531557ad40cc
Student Free <andreevdamir266@gmail.com>
2024-05-14 11:08:13 +0300

    коммит11.2(2)

--- src/decorators.py
+++ src/decorators.py
@@ -1,16 +1,21 @@
 from functools import wraps
 from datetime import datetime
 from typing import Callable, Any
+import os
 
 
 def log(func: Callable) -> Callable:
     """Декоратор, который логирует вызов функции с именем и временем."""
+
     @wraps(func)
-    def wrapper(*args: Any, **kwargs: Any) -> Any:
-        result_num = func(*args, **kwargs)
+    def wrapper(args: Any, *kwargs: Any) -> Any:
+        result_num = func(args, *kwargs)
         current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        with open('mylog.txt', 'a', encoding='utf-8') as log_file:
-            log_file.write(f'{current_time} {func.__name__} ok\n')
+        try:
+            with open('mylog.txt', 'a', encoding='utf-8') as log_file:
+                log_file.write(f'{current_time} {func.__name__} ok\n')
+        except FileNotFoundError:
+            print(f'{current_time} {func.__name__} ok')
         return result_num
 
     return wrapper
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -6,3 +6,15 @@ def test_my_function() -> None:
     assert my_function(3, 4) == 7
     assert my_function(4, 5) == 9
     assert my_function(1, 2) == 3
+
+
+
+
+
+
+
+
+
+        
+
+
